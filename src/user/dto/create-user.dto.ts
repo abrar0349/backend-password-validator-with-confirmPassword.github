a2a32d47a1e16e234e93ba9b12c30189a350f88b
jawad <usmankhan26112007@gmail.com>
2023-07-07 17:09:24 +0500

    password match with confirmPassword and and some restriction mean should have special character like @ etc

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,46 @@
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsNumber,
+    IsString,
+    Matches,
+    MaxLength,
+    MinLength,
+    Validate,
+  } from 'class-validator';
+  import { MatchConstraint } from 'src/comman/decorators/match-properties.decorator';
+  
+  export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
+    firstName: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    lastName: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    @IsEmail({}, { message: 'Invalid email address' })
+    email: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(15)
+    @Matches(/^(?=.*[A-Z])(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,15}$/, {
+      message: "password expectation doesn't meet",
+    })
+    password: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    @Validate(MatchConstraint, ['password'])
+    confirmPassword: string;
+  
+    @IsNotEmpty()
+    @IsNumber()
+    age: number;
+  
+  }
+  
